2579700e2660c3a26d507878cf1e17f1f14dedde
xiao <xiaoshiao1999@163.com>
2020-07-01 10:52:13 +0800

    ten

--- src/Text01.java
+++ src/Text01.java
@@ -1,6 +1,6 @@
 public class Text01 {
     public static void main(String[] args) {
         int ssf;
-        System.out.println("Hello World，today is day02");
+        System.out.println("Hello World，today is day023333");
     }
 }
